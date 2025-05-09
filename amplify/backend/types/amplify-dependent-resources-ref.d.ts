69dfce4ec7fa906399bd1cdcce9eb903a372a19f
Todd Williams <tswilliams87@gmail.com>
2025-05-08 22:42:36 -0500

    added storage to amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "localmilkaa608e10": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3e36cdd9d": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
